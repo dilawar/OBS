2c8e5c2d37574aa6b6c6e9a511c4f83fa94f7d5f
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-18 01:49:02 +0530

    Disabling moose-doc and moose-moogli everywhere. Add them later.

--- moose/build.sh
+++ moose/build.sh
@@ -6,4 +6,8 @@ echo "Building for repo $1"
 if [ ! -f ./_service:recompress:tar_scm:moose-3.0.2.tar.gz ]; then
     osc service run
 fi
-osc build --noservice $1 moose-$1.spec | tee __build_$1.log
+if [[ "$1" == *"Debian"* ]]; then
+    osc build --noservice $1 | tee __build_$1.log
+else
+    osc build --noservice $1 moose-$1.spec | tee __build_$1.log
+fi
